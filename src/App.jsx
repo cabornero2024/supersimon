936b31f5a1a91b97658113912fa2a10d0ddba609
DAW1 <jon.cabornero@ikasle.aeg.eus>
2024-03-14 11:37:25 +0100

    Secuencia Array

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import './App.css'
 
 function App() {
   const [colors, setcolor] = useState(['#2f83dc', '#d7dc2f', '#49dc2f', '#dc342f']);
+  const [sequence, setSequence] = useState([]);
   const handleClick = (index) => {
-    console.log(index);
+    randonNumber();
   }
+
+  const randonNumber = () => {
+    const minNumber = 0;
+    const maxNumber = 3;
+    const randonNumber = Math.floor(Math.random() * (maxNumber - minNumber +1) + minNumber);
+    setSequence([...sequence, randonNumber]);
+   
+  }
+
   return (
     <>
     <div className=''> 
     <h1>SuperSimon</h1>
+    <p>{sequence}</p>
     </div>
     <div className='container'>
     {
@@ -39,8 +50,8 @@ export default App
    
    /*1.-Interfaz de carton SOLVED*/
    /*2.-Detectar click en los componentes SOLVED*/
-   /*3.-Crear Array para almacenar la secuencia del juego
-   /*4.-Crear el color random para añadirlo a la secuencia
+   /*3.-Crear Array para almacenar la secuencia del juego SOLVED
+   /*4.-Crear el color random para añadirlo a la secuencia SOLVED
    /*5.-Reproducir la secuencia - HARD
    /*6.-Dejar que el usuario intente reproducir la secuencia
    /*7.-Validar secuencia usuario por cada pulsacion
